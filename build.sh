0894349ec8521aeab100f8363183c805a604a5df
Faraz Khan <khanfaraz1199@gmail.com>
2025-06-18 02:54:22 -0500

    Remove .env in build.sh file

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ npm install
 rm -rf lambda.zip
 
 echo "Creating deployment package..."
-zip -r lambda.zip src index.js .env package.json package-lock.json node_modules README.md LICENSE
+zip -r lambda.zip src index.js package.json package-lock.json node_modules README.md LICENSE
 
 echo "✅ lambda.zip created successfully!"
